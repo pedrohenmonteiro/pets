075e5178208e8a9125309fc4bb5a09d8b3fa123f
pedrohenmonteiro <pedromont436@gmail.com>
2022-12-02 21:36:08 -0300

    boilerplate

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<div>Teste</div>} />
+    <Route path="/" element={<div className="text-violet-500">Teste</div>} />
     <Route path="/movie/:id" element={<div>Teste de página</div>} />
   </Routes>
 );
